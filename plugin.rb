88c59c3074649bf3155ea8e74de7a515e470615b
liu <liuhe36@gmail.com>
2024-03-21 20:04:02 +0800

    DEL

--- plugin.rb
+++ plugin.rb
@@ -1,4 +1,8 @@
-# plugin.rb
+# name: discourse-wechat-article
+# about: Discourse plugin for wechat article
+# version: 0.0.1
+# authors: liu
+# url: https://github.com/xinfinite-net/discourse-wechat-article
 after_initialize do
 
   # 在主题视图渲染前修改 @topic_view_posts
