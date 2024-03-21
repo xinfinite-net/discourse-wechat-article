ae00f67afc62fc969475d74a0f68bcaa95fe1ac5
liu <liuhe36@gmail.com>
2024-03-21 20:34:45 +0800

    DEL

---
+++ lib/white_lister.rb
@@ -0,0 +1,10 @@
+# lib/white_lister.rb
+module WhitelistedHelper
+  def self.all_whitelist
+    # 获取默认白名单
+    whitelist = DiscourseNgFilter.select_whitelister_args[:whitelisted]
+
+    # 修改时返回新白名单，保留默认
+    whitelist
+  end
+end
--- plugin.rb
+++ plugin.rb
@@ -1,17 +1,21 @@
 # name: discourse-wechat-article
 # about: Discourse plugin for wechat article
-# version: 0.0.1
+# version: 0.0.2
 # authors: liu
 # url: https://github.com/xinfinite-net/discourse-wechat-article
 after_initialize do
 
-  # 在主题视图渲染前修改 @topic_view_posts
-  add_to_class(:topic_view, :pre_rendered_data) do |posts|
-    posts.each do |post|
-      # 假设你有一个特定的用户id
-      if post.user_id == 1
-        post.cooked = post.raw # 用原始HTML替换cooked内容
-      end
+  # 设置默认白名单
+  whitelist = WhitelistedHelper.all_whitelist
+
+  # 从配置文件获取特殊用户列表
+  special_usernames = settings.special_usernames.split('|')
+
+  # 在request周期内检查是否为特殊用户
+  Tracker.on(:before_rendering) do |info|
+    if special_usernames.include?(info[:request].env["HTTP_DISCOURSE_USERNAME"])
+      whitelist[:attributes]['style'] = :relaxed  # 允许style属性
+      info[:request].env["WHITE_LISTER"] = whitelist
     end
   end
 end
