b4c191b05a2703c9904b9f3024a18700b2f1169b
liuhe36 <liuhe36@gmail.com>
2024-03-21 19:48:43 +0800

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,9 +1,13 @@
-# name: discourse-hearthstone
-# about: Hearthstone integration
-# version: 0.0.1
-# authors: Rafael Silva
-# url: https://github.com/xfalcox/discourse-hearthstone
+# plugin.rb
+after_initialize do
 
-enabled_site_setting :discourse_hearthstone_enabled
-
-register_asset "stylesheets/d-tooltip.scss"
+  # 在主题视图渲染前修改 @topic_view_posts
+  add_to_class(:topic_view, :pre_rendered_data) do |posts|
+    posts.each do |post|
+      # 假设你有一个特定的用户id
+      if post.user_id == 1
+        post.cooked = post.raw # 用原始HTML替换cooked内容
+      end
+    end
+  end
+end
