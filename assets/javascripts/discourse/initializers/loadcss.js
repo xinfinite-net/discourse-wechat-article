6b89590a04143ee952535f659153d28095754d82
liu <liuhe36@gmail.com>
2024-04-01 10:45:31 +0800

    DEV

--- assets/javascripts/discourse/initializers/loadcss.js
+++ assets/javascripts/discourse/initializers/loadcss.js
@@ -1,5 +1,7 @@
-import {loadScript, loadCSS} from "discourse/lib/load-script";
+// import {loadScript, loadCSS} from "discourse/lib/load-script";
 import {withPluginApi} from "discourse/lib/plugin-api";
+import {ajax} from "discourse/lib/ajax";
+
 
 export default {
     name: "wechat-load-css",
@@ -8,14 +10,43 @@ export default {
         withPluginApi("0.8.22", api => {
             api.decorateCookedElement($elem => {
                 const controller = Discourse.__container__.lookup("controller:topic")
-                const username = controller.get("model.details.created_by.username")
                 const externalId = controller.get("model.external_id")
                 if (externalId) {
-                    loadCSS("http://xinfinite1.cedu.ac.cn/wct-cr-css/" + externalId + ".css").then(() => {
-                        console.log("Mermaid loaded", $elem);
-                        console.log("controller", controller);
-                        console.log("username", username);
-                        console.log("username", externalId);
+
+                    function loadCSS_wct(url) {
+                        return new Promise(function (resolve) {
+
+                            function ensureStyleWithId(id) {
+                                let style = document.getElementById(id);
+                                if (!style) {
+                                    style = document.createElement('style');
+                                    style.id = id;
+                                    document.head.appendChild(style);
+                                }
+                                return style;
+                            }
+
+                            // 更新style标签的内容
+                            function updateStyleContent(id, newContent) {
+                                const style = ensureStyleWithId(id);
+                                style.innerHTML = newContent;
+                            }
+
+                            const cb = function (data) {
+                                ensureStyleWithId("wct-cr-css");
+                                updateStyleContent("wct-cr-css", data);
+                                resolve();
+                            };
+
+                            ajax({
+                                url: url,
+                                dataType: "text",
+                            }).then(cb);
+                        });
+                    }
+
+                    loadCSS_wct("http://xinfinite1.cedu.ac.cn/wct-cr-css/" + externalId + ".css").then(() => {
+                        console.log("CSS loaded");
                     });
                 }
 
@@ -24,4 +55,5 @@ export default {
             });
         });
     }
+
 };
