aaf51984c209fe12f0914882d09e7d567ba289f5
liu <liuhe36@gmail.com>
2024-04-09 22:33:02 +0800

    DEV

--- assets/javascripts/discourse/discourse-markdown/whitelist.js
+++ assets/javascripts/discourse/discourse-markdown/whitelist.js
@@ -2,4 +2,5 @@ export function setup(helper) {
     if (!helper.markdownIt) { return; }
     helper.whiteList(["p[data-*]"]);
     helper.whiteList(["img[data-*]"]);
+    helper.whiteList(["video[data-*]"]);
 }
